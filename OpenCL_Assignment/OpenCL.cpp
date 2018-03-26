4866743a8a6c7a7e01e900d3c6b804ddf6cd949f
TatendaMugadza <mugadzat8@gmail.com>
2018-03-26 12:28:31 +0200

    Added the structure to OpenCL wrapper.

--- OpenCL_Assignment/OpenCL.cpp
+++ OpenCL_Assignment/OpenCL.cpp
@@ -1,5 +1,49 @@
 #include "stdafx.h"
 #include "OpenCL.h"
 
+bool OpenCL_Init(const char * Vendor)
+{
+	return false;
+}
 
+void OpenCL_Destroy() {
 
+}
+
+bool OpenCL_LoadKernel(const char * Filename, const char * Function)
+{
+	return false;
+}
+
+cl_mem OpenCL_CreateBuffer(cl_uint Argument, cl_mem_flags Flags, size_t Size)
+{
+	return cl_mem();
+}
+
+void OpenCL_FreeBuffer(cl_mem Buffer)
+{
+}
+
+bool OpenCL_ConstantInt(cl_uint Argument, int Value)
+{
+	return false;
+}
+
+bool OpenCL_ReadData(cl_mem Buffer, size_t Size, void * Data)
+{
+	return false;
+}
+
+bool OpenCL_WriteData(cl_mem Buffer, size_t Size, void * Data)
+{
+	return false;
+}
+
+bool OpenCL_Run(int N, size_t * LocalSize)
+{
+	return false;
+}
+
+void OpenCL_PrepareLocalSize(int N, size_t * LocalSize)
+{
+}
--- OpenCL_Assignment/OpenCL.h
+++ OpenCL_Assignment/OpenCL.h
@@ -16,6 +16,25 @@
   *						Includes
   *****************************************************************/
 
+  /*****************************************************************
+  *						Function Declarations
+  *****************************************************************/
+bool OpenCL_Init(const char* Vendor);
+void OpenCL_Destroy();
+
+bool OpenCL_LoadKernel(const char* Filename, const char* Function);
+
+cl_mem OpenCL_CreateBuffer(cl_uint Argument, cl_mem_flags Flags, size_t Size);
+void   OpenCL_FreeBuffer(cl_mem Buffer);
+
+bool OpenCL_ConstantInt(cl_uint Argument, int Value);
+
+bool OpenCL_ReadData(cl_mem Buffer, size_t Size, void* Data);
+bool OpenCL_WriteData(cl_mem Buffer, size_t Size, void* Data);
+
+bool OpenCL_Run(int N, size_t* LocalSize);
+
+void OpenCL_PrepareLocalSize(int N, size_t* LocalSize);
 
 
 #endif // !OPENCL_H
